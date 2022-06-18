a0240de47187455e74b7a88f1709aa65b2a19bc5
Shah Zahid <shahzaahid038@gmail.com>
2022-06-18 21:38:57 +0530

    insert the new line in hello

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ print("This is the third line in vs code")
 print("This is the 4th line in vs code")
 print("This is the 5th line in vs code")
 print("zahid")
+print("This is the another line in vs code")
 
 
 
