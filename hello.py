9eb5833f17c359e96dd1870b5b2a47a984961238
Shah Zahid <shahzaahid038@gmail.com>
2022-06-18 20:47:32 +0530

    I did delete the one line

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ print("This is the second line in vs code")
 print("This is the third line in vs code")
 print("This is the 4th line in vs code")
 print("This is the 5th line in vs code")
-print("This is the next line in vs code")
+
+
 
 
